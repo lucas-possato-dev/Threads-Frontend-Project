7aecc95a8d9eae42bd518e870b2d908137f62f1a
lucas-possato-dev <lucaspstinho@gmail.com>
2023-07-31 12:05:56 -0300

    userPage done

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -5,7 +5,24 @@ const UserPage = () => {
   return (
     <>
       <UserHeader />
-      <UserPost />
+      <UserPost
+        likes={"5K"}
+        replies={866}
+        postImg={"/post1.png"}
+        postTitle={"Hello, world!"}
+      />
+      <UserPost
+        likes={"5K"}
+        replies={866}
+        postImg={"/threads.png"}
+        postTitle={"Let's talk about Threads."}
+      />
+      <UserPost
+        likes={"5K"}
+        replies={866}
+        postImg={"/post3.png"}
+        postTitle={"He doesn't look happy."}
+      />
     </>
   );
 };
